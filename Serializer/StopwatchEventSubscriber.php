9e4caf2b6a481821bf1ba2c7b9c1b52e766b28c4
Adrien Brault <adrien.brault@gmail.com>
2013-10-15 11:09:13 -0700

    Integrate serialization with the stopwatch

---
+++ Serializer/StopwatchEventSubscriber.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace JMS\SerializerBundle\Serializer;
+
+use JMS\Serializer\EventDispatcher\Events;
+use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
+use JMS\Serializer\EventDispatcher\ObjectEvent;
+use Symfony\Component\Stopwatch\Stopwatch;
+
+/**
+ * @author Adrien Brault <adrien.brault@gmail.com>
+ */
+class StopwatchEventSubscriber implements EventSubscriberInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            array('event' => Events::PRE_SERIALIZE, 'method' => 'onPreSerialize', 'priority' => -1000),
+            array('event' => Events::POST_SERIALIZE, 'method' => 'onPostSerialize', 'priority' => 1000),
+        );
+    }
+
+    /**
+     * @var Stopwatch
+     */
+    private $stopwatch;
+
+    /**
+     * @var object
+     */
+    private $rootObject;
+
+    public function __construct(Stopwatch $stopwatch)
+    {
+        $this->stopwatch = $stopwatch;
+    }
+
+    public function onPreSerialize(ObjectEvent $event)
+    {
+        if ($event->getContext()->getDepth() > 1 || null !== $this->rootObject) {
+            return;
+        }
+
+        $this->stopwatch->start('jms_serializer');
+        $this->rootObject = $event->getObject();
+    }
+
+    public function onPostSerialize(ObjectEvent $event)
+    {
+        if (null === $this->rootObject || $event->getObject() !== $this->rootObject) {
+            return;
+        }
+
+        $this->stopwatch->stop('jms_serializer');
+        $this->rootObject = null;
+    }
+}
--- Tests/DependencyInjection/JMSSerializerExtensionTest.php
+++ Tests/DependencyInjection/JMSSerializerExtensionTest.php
@@ -167,6 +167,7 @@ class JMSSerializerExtensionTest extends \PHPUnit_Framework_TestCase
         $container->set('annotation_reader', new AnnotationReader());
         $container->set('service_container', $container);
         $container->set('translator', $this->getMock('Symfony\\Component\\Translation\\TranslatorInterface'));
+        $container->set('debug.stopwatch', $this->getMock('Symfony\\Component\\Stopwatch\\Stopwatch'));
         $container->registerExtension($extension);
         $extension->load($configs, $container);
 
