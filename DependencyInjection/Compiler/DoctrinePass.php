47ed496960a3123e4f9a63b8b3de6caeaa8cd8a4
Uwe Jäger <uwej711@googlemail.com>
2014-05-28 16:43:58 +0200

    fix chaining of metadata driver and object constructor

--- DependencyInjection/Compiler/DoctrinePass.php
+++ DependencyInjection/Compiler/DoctrinePass.php
@@ -31,25 +31,26 @@ class DoctrinePass implements CompilerPassInterface
         }
 
         $serviceTemplates = array(
-            'jms_serializer.metadata_driver' => 'jms_serializer.metadata.%s_type_driver',
-            'jms_serializer.object_constructor' => 'jms_serializer.%s_object_constructor',
+            'jms_serializer.metadata_driver' => array('template' => 'jms_serializer.metadata.%s_type_driver', 'position' => 0),
+            'jms_serializer.object_constructor' => array('template' => 'jms_serializer.%s_object_constructor', 'position' => 1)
         );
 
         $registry = array_pop($registries);
         $previousId = array();
-        foreach ($serviceTemplates as $service => $serviceTemplate) {
-            $previousId[$service] = sprintf($serviceTemplate, $registry);
-            $container->setAlias($service, $previousId[$service]);
+        foreach ($serviceTemplates as $serviceName => $service) {
+            $previousId[$serviceName] = sprintf($service['template'], $registry);
+            $container->setAlias($serviceName, $previousId[$serviceName]);
         }
 
         foreach ($registries as $registry) {
-            foreach ($serviceTemplates as $service => $serviceTemplate) {
-                $id = sprintf($serviceTemplate, $registry);
+            foreach ($serviceTemplates as $serviceName => $service) {
+                $id = sprintf($service['template'], $registry);
                 $container
                     ->getDefinition($id)
-                    ->replaceArgument(0, new Reference($previousId[$service]))
+                    ->replaceArgument($service['position'], new Reference($previousId[$serviceName]))
                 ;
-                $previousId[$service] = $id;
+                $previousId[$serviceName] = $id;
+                $container->setAlias($serviceName, $previousId[$serviceName]);
             }
         }
     }
