53c4fddcabd7e9316e48fe5ea0d86b4e64699e38
Asmir Mustafic <goetas@gmail.com>
2017-03-15 10:12:32 +0100

    allow class to be a symfony parameter

---
+++ Tests/DependencyInjection/Fixture/EventSubscriber.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * Copyright 2011 Johannes M. Schmitt <schmittjoh@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace JMS\SerializerBundle\Tests\DependencyInjection\Fixture;
+
+use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
+
+class EventSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return array(
+            array(
+                'event' => 'serializer.pre_serialize',
+                'class' => 'Bar',
+                'format' => 'json'
+            ),
+        );
+    }
+}
