2ff3ae37e96c62284878dc16a2fe3d0720fe6630
Asmir Mustafic <goetas@gmail.com>
2017-03-15 09:23:15 +0100

    Fixed path tests

--- Tests/DependencyInjection/ConfigurationTest.php
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -41,6 +41,7 @@ class ConfigurationTest extends \PHPUnit_Framework_TestCase
 
     public function testConfig()
     {
+        $ref = new JMSSerializerBundle();
         $container = $this->getContainer([
             [
                 'metadata' => [
@@ -59,7 +60,8 @@ class ConfigurationTest extends \PHPUnit_Framework_TestCase
         ]);
 
         $directories = $container->getDefinition('jms_serializer.metadata.file_locator')->getArgument(0);
-        $this->assertEquals('/home/goetas/projects/serializer-bundle', $directories['JMSSerializerBundleNs1']);
-        $this->assertEquals('/home/goetas/projects/serializer-bundle/Resources/config', $directories['JMSSerializerBundleNs2']);
+
+        $this->assertEquals($ref->getPath(), $directories['JMSSerializerBundleNs1']);
+        $this->assertEquals($ref->getPath().'/Resources/config', $directories['JMSSerializerBundleNs2']);
     }
 }
