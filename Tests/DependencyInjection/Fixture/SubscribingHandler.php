2365caadafd8a769466d4217139160bbe0d0f8dd
Asmir Mustafic <goetas@gmail.com>
2018-04-20 08:37:17 +0200

    update tests

--- Tests/DependencyInjection/Fixture/SubscribingHandler.php
+++ Tests/DependencyInjection/Fixture/SubscribingHandler.php
@@ -2,7 +2,7 @@
 
 namespace JMS\SerializerBundle\Tests\DependencyInjection\Fixture;
 
-use JMS\Serializer\GraphNavigator;
+use JMS\Serializer\GraphNavigatorInterface;
 use JMS\Serializer\Handler\SubscribingHandlerInterface;
 
 class SubscribingHandler implements SubscribingHandlerInterface
@@ -11,7 +11,7 @@ class SubscribingHandler implements SubscribingHandlerInterface
     {
         return array(
             array(
-                'direction' => GraphNavigator::DIRECTION_SERIALIZATION,
+                'direction' => GraphNavigatorInterface::DIRECTION_SERIALIZATION,
                 'format' => 'json',
                 'type' => 'DateTime',
                 'method' => 'onDateTime',
