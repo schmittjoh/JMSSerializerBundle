4102cb72e011e38237b2ce28b56433811ac68c75
Alexander Strizhak <gam6itko@gmail.com>
2021-03-07 23:04:45 +0300

    add CacheClearer (#839)
    * add CacheClearer

---
+++ Cache/CacheClearer.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JMS\SerializerBundle\Cache;
+
+use Metadata\Cache\CacheInterface;
+use Metadata\Cache\ClearableCacheInterface;
+use Symfony\Component\HttpKernel\CacheClearer\CacheClearerInterface;
+
+/**
+ * @author Alexander Strizhak <gam6itko@gmail.com>
+ */
+class CacheClearer implements CacheClearerInterface
+{
+    /**
+     * @var CacheInterface|ClearableCacheInterface
+     */
+    private $cache;
+
+    /**
+     * @param CacheInterface|ClearableCacheInterface $cache
+     */
+    public function __construct(CacheInterface $cache)
+    {
+        $this->cache = $cache;
+    }
+
+    public function clear(string $cacheDirectory): void
+    {
+        if ($this->cache instanceof ClearableCacheInterface) {
+            $this->cache->clear();
+        }
+    }
+}
