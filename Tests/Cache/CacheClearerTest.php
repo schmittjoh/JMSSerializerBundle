fc74a8ee86a7aac010fb1272e983eb3e971374f3
Alexander Strizhak <gam6itko@gmail.com>
2021-03-21 21:50:47 +0300

    rewrite CacheClearerTest according to --prefer-lowest tests (#842)
    * rewrite CacheClearerTest according to --prefer-lowest tests
    
    * rewrite CacheClearer according to --prefer-lowest tests

--- Tests/Cache/CacheClearerTest.php
+++ Tests/Cache/CacheClearerTest.php
@@ -7,19 +7,17 @@ namespace JMS\SerializerBundle\Tests\Cache;
 use JMS\SerializerBundle\Cache\CacheClearer;
 use Metadata\Cache\PsrCacheAdapter;
 use PHPUnit\Framework\TestCase;
-use Psr\Cache\CacheItemPoolInterface;
 
 class CacheClearerTest extends TestCase
 {
     public function testClear(): void
     {
-        $cache = $this->createMock(CacheItemPoolInterface::class);
-        $cache
+        $adapter = $this->createMock(PsrCacheAdapter::class);
+        $adapter
             ->expects(self::once())
             ->method('clear')
             ->willReturn(true);
 
-        $adapter = new PsrCacheAdapter('', $cache);
         $clearer = new CacheClearer($adapter);
         $clearer->clear('');
     }
