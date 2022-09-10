d08033a98b0ce4cec036c5c0f0fea9779a6efe1f
Marcin Czarnecki <marcin.czarnecki@westwing.pl>
2022-09-10 20:13:32 +0200

    Stop collecting metadata from cache

---
+++ Debug/TraceableMetadataFactory.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+
+namespace JMS\SerializerBundle\Debug;
+
+use Metadata\AdvancedMetadataFactoryInterface;
+use Metadata\ClassMetadata;
+
+/**
+ * @internal
+ */
+final class TraceableMetadataFactory implements AdvancedMetadataFactoryInterface
+{
+    private $storage = [];
+    
+    /**
+     * @var AdvancedMetadataFactoryInterface
+     */
+    private $metadataFactory;
+
+    public function __construct(AdvancedMetadataFactoryInterface $metadataFactory)
+    {
+        $this->metadataFactory = $metadataFactory;
+    }
+
+    public function getLoadedMetadata(): array
+    {
+        return $this->storage;
+    }
+    
+    public function getAllClassNames(): array
+    {
+        return $this->metadataFactory->getAllClassNames();
+    }
+
+    public function getMetadataForClass(string $className)
+    {
+        $metadata = $this->metadataFactory->getMetadataForClass($className);
+        if ($metadata instanceof ClassMetadata) {
+            $this->trackMetadata($metadata);
+        }
+        
+        return $metadata;
+    }
+    
+    protected function trackMetadata(ClassMetadata $metadata): void
+    {
+        $class = $metadata->name;
+        $this->storage[$class] = array_merge(
+            $this->storage[$class] ?? [], $metadata->fileResources
+        );
+        $this->storage[$class] = array_unique($this->storage[$class]);
+    }
+}
